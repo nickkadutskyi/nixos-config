c4e2767a060fac5283a4727fa1f769e2b94af5ef
Nick Kadutskyi <nick@kadutskyi.com>
2024-12-18 17:49:17 -0800

    Adds computer name

--- machines/Nicks-Mac-mini.nix
+++ machines/Nicks-Mac-mini.nix
@@ -3,4 +3,6 @@
   imports = [
     ./aarch64-darwin-shared.nix
   ];
+  networking.computerName = "Nick's Mac mini";
+  networking.hostName = "Nicks-Mac-mini";
 }
--- machines/Nicks-MacBook-Air.nix
+++ machines/Nicks-MacBook-Air.nix
@@ -3,4 +3,7 @@
   imports = [
     ./aarch64-darwin-shared.nix
   ];
+  networking.computerName = "Nick's MacBook Air";
+  networking.hostName = "Nicks-MacBook-Air";
+  networking.dns = [ ];
 }
