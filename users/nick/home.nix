16821739bc8090b9349dcc56b1fd7b6e126ae997
Nick Kadutskyi <nick@kadutskyi.com>
2025-01-07 01:27:41 -0800

    Switches to starship and removes oh-my-zhs

--- users/nick/home.nix
+++ users/nick/home.nix
@@ -140,8 +140,6 @@ in
       wget
       # Suggests entries from history with grey text
       zsh-autosuggestions
-      # p10k prompt
-      zsh-powerlevel10k
       # Highlights binaries in terminal emulator
       zsh-syntax-highlighting
     ]
@@ -408,11 +406,12 @@ in
   # Programs
   #---------------------------------------------------------------------
 
-  # TODO fix scripts and ensure key bindings work properly or get rid of tmux
   programs.alacritty = {
     enable = !isWSL;
     settings = import ./alacritty/alacritty.nix { inherit lib pkgs; };
   };
+
+  programs.bash.enable = true;
 
   # Enables direnv to automatically switch environments in project directories.
   programs.direnv = {
@@ -505,16 +504,96 @@ in
     };
   };
 
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = false;
+      command_timeout = 1000;
+      format = lib.concatStrings [
+        "$username"
+        "$hostname"
+        "$directory"
+        "$git_branch"
+        "$git_state"
+        "$git_status"
+        "$nix_shell"
+        "$direnv"
+        "$shlvl"
+        "$sudo"
+        "$cmd_duration"
+        "$line_break"
+        "$character"
+      ];
+      username = {
+        format = "[$user]($style) at ";
+      };
+      hostname = {
+        format = "[$ssh_symbol$hostname]($style) ";
+        ssh_symbol = "󰌘 ";
+      };
+      directory = {
+        style = "blue";
+      };
+      character = {
+        success_symbol = "[❯](purple)";
+        error_symbol = "[❯](red)";
+        vimcmd_symbol = "[❮](green)";
+      };
+      git_branch = {
+        format = "[$branch]($style)";
+        style = "bright-black";
+      };
+      git_status = {
+        format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](bright-black) ($ahead_behind$stashed)]($style) ";
+        style = "cyan";
+        conflicted = "​";
+        untracked = "​";
+        modified = "​";
+        staged = "​";
+        renamed = "​";
+        deleted = "​";
+        stashed = "≡";
+      };
+      git_state = {
+        format = "\([$state( $progress_current/$progress_total)]($style)\) ";
+        style = "bright-black";
+      };
+      nix_shell = {
+        format = "[$symbol$state(\\($name\\))]($style) ";
+        style = "blue";
+        symbol = "󱄅 ";
+        heuristic = true;
+        impure_msg = "󰻍 ";
+        pure_msg = " ";
+      };
+      direnv = {
+        format = "[$loaded]($style) ";
+        disabled = false;
+        style = "yellow";
+        loaded_msg = "󰏖 ";
+        unloaded_msg = "󰏗 ";
+      };
+      shlvl = {
+        disabled = false;
+        symbol = " ";
+        style = "yellow";
+      };
+      sudo = {
+        disabled = false;
+        format = "[$symbol]($style) ";
+        symbol = "󰠠 ";
+        style = "yellow";
+      };
+      cmd_duration = {
+        style = "yellow";
+      };
+    };
+  };
+
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-    oh-my-zsh.enable = true;
     plugins = [
-      {
-        name = "powerlevel10k";
-        src = pkgs.zsh-powerlevel10k;
-        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-      }
       {
         name = "zsh-autosuggestions";
         src = pkgs.zsh-autosuggestions;
@@ -550,25 +629,7 @@ in
       # bash
       ''
         ${builtins.readFile ./zshrc}
-        ${builtins.readFile ./p10k.zsh}
-      ''
-      + (
-        if config.programs.zsh.oh-my-zsh.enable then
-          # bash
-          ''
-            # tmux title start
-            # Uses OMZ theme terminal title directives for tmux
-            function omz_termsupport_precmd_tmux_extended() {
-              if [[ "$TERM" =~ "tmux*"  ]]; then
-                print -Pn "\e]2;''${ZSH_THEME_TERM_TITLE_IDLE:q}\e\\"
-              fi
-            }
-            add-zsh-hook precmd omz_termsupport_precmd_tmux_extended
-            # tmux title end
-          ''
-        else
-          ""
-      );
+      '';
   };
 
   #---------------------------------------------------------------------
