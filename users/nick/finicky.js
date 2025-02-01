35f2c6b5e3c79f3157e5bc367213749191e1d41d
Nick Kadutskyi <nick@kadutskyi.com>
2025-02-01 11:40:51 -0800

    Changes code style for JS files

--- users/nick/finicky.js
+++ users/nick/finicky.js
@@ -1,16 +1,16 @@
 module.exports = {
-	defaultBrowser: "Safari",
-	options: {
-		hideIcon: true,
-	},
-	rewrite: [
-		{
-			match: "https://app.clickup.com/*",
-			url: ({ url }) => ({
-				...url,
-				host: "",
-				protocol: "clickup"
-			})
-		},
-	]
+  defaultBrowser: "Safari",
+  options: {
+    hideIcon: true,
+  },
+  rewrite: [
+    {
+      match: "https://app.clickup.com/*",
+      url: ({ url }) => ({
+        ...url,
+        host: "",
+        protocol: "clickup"
+      })
+    },
+  ]
 };
