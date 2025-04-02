b92dcc76ad95ac50b3f2e902dbf29f473df5946a
Nick Kadutskyi <nick@kadutskyi.com>
2025-04-01 20:09:29 -0700

    refactor: replaces nix package

--- machines/aarch64-darwin-shared.nix
+++ machines/aarch64-darwin-shared.nix
@@ -11,7 +11,7 @@
   # We install Nix using a separate installer so we don't want nix-darwin
   # to manage it for us. This tells nix-darwin to just use whatever is running.
   nix = {
-    package = pkgs.nixVersions.nix_2_26;
+    package = pkgs.nixVersions.latest;
     settings = {
       # Enables flakes
       experimental-features = [
