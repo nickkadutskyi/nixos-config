de65b19d7f84ebf29bb1e19cb637c7b36124500c
Nick Kadutskyi <nick@kadutskyi.com>
2025-06-22 17:53:41 -0700

    adjusts git status in cmd prompt

--- users/nick/starship/starship.nix
+++ users/nick/starship/starship.nix
@@ -41,8 +41,8 @@
     style = "bright-black";
   };
   git_status = {
-    format = "[[( 󰇂$conflicted$untracked$modified$staged$renamed$deleted)](bright-black) ($ahead_behind$stashed)]($style) ";
-    style = "cyan";
+    format = "[( $ahead_behind$stashed)[( 󰇂$conflicted$untracked$modified$staged$renamed$deleted)](bright-blue)]($style) ";
+    style = "bright-black";
     conflicted = "​";
     untracked = "​";
     modified = "​";
@@ -52,6 +52,7 @@
     stashed = "≡";
     behind = "󰦸";
     ahead = "󰧆";
+    diverged = "󰦸󰧆";
   };
   git_state = {
     format = "\([$state($progress_current/$progress_total)]($style)\) ";
