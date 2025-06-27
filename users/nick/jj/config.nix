344d1eb2382a4d030b3dd865bf962ef20bd4fe0e
Nick Kadutskyi <nick@kadutskyi.com>
2025-06-26 21:57:52 -0700

    jj: neovim as diff tool

--- users/nick/jj/config.nix
+++ users/nick/jj/config.nix
@@ -62,4 +62,25 @@
 
   [git]
   push-new-bookmarks = true
+
+  [merge-tools.neovim]
+  program = "sh"
+  edit-args = [
+    "-c",
+    """
+      set -eu
+      rm -f "$right/JJ-INSTRUCTIONS"
+      git -C "$left" init -q
+      git -C "$left" add -A
+      git -C "$left" commit -q -m baseline --allow-empty # create parent commit
+      mv "$left/.git" "$right"
+      git -C "$right" add --intent-to-add -A # create current working copy
+      (cd "$right"; nvim -c "lua require('lazy').load({plugins = {'diffview.nvim'}})" -c DiffviewOpen)
+      git -C "$right" diff-index --quiet --cached HEAD && { echo "No changes done, aborting split."; exit 1; }
+      git -C "$right" commit -q -m split # create commit on top of parent including changes
+      git -C "$right" restore . # undo changes in modified files
+      git -C "$right" reset .   # undo --intent-to-add
+      git -C "$right" clean -q -df # remove untracked files
+    """
+  ]
 ''
