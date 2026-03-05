e2362b75f8e38153979e43249b553c269ea089e4
Nick Kadutskyi <nick@kadutskyi.com>
2026-03-05 13:06:01 -0800

    update

--- systems/darwin.nix
+++ systems/darwin.nix
@@ -31,7 +31,7 @@
   };
 
   environment.systemPackages = [
-    inputs.neovim-nightly-overlay.packages.${pkgs.system}.default
+    inputs.neovim-nightly-overlay.packages.${pkgs.stdenv.hostPlatform.system}.default
     # pkgs.neovim
   ];
 
