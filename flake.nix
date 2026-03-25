1edb8ed5a64bdf8d25afff589f2d8abe7dd51e33
Nick Kadutskyi <nick@kadutskyi.com>
2026-03-25 15:30:11 -0700

    fix

--- flake.nix
+++ flake.nix
@@ -83,6 +83,9 @@
         (final: prev: rec {
           # csvkit 2.2.0 on unstable won't build so using 2.1.0 from stable
           # csvkit = nixpkgs-stable.legacyPackages.${prev.stdenv.hostPlatform.system}.csvkit;
+          # Direnv is currently broken and PR is not in nixpkgs-unstable yet
+          direnv = nixpkgs-master.legacyPackages.${prev.stdenv.hostPlatform.system}.direnv;
+          nix-direnv = nixpkgs-master.legacyPackages.${prev.stdenv.hostPlatform.system}.nix-direnv;
           # Latest opencode from master nixpkgs
           # opencode = nixpkgs-master.legacyPackages.${prev.stdenv.hostPlatform.system}.opencode;
           # dark-mode-notify from nixpkgs-unstable doesn't work due to failed build of swift-5.10.1
